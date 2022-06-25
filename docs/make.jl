d1458aa7820bf8d0193c259aa109ca287b73eaa0
Bryson Cale <bryson.cale1@gmail.com>
2022-06-25 08:09:42 -0700

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,12 @@
-pushfirst!(LOAD_PATH,"../src/")
+#pushfirst!(LOAD_PATH,"../src/")
+
+using Pkg
+Pkg.add(url="https://github.com/astrobc1/CurveFitParameters.jl")
+Pkg.add(url="https://github.com/astrobc1/IterativeNelderMead.jl")
+Pkg.add(url="https://github.com/astrobc1/EchelleBase.jl")
+Pkg.add(url="https://github.com/astrobc1/EchelleReduce.jl")
+Pkg.add(url="https://github.com/astrobc1/EchelleSpectralModeling.jl")
+Pkg.add(url="https://github.com/astrobc1/Echelle.jl")
 
 using Documenter
 using Echelle
