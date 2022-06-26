d40c3ed14aeb946397d4979adf8da7bd80ae7702
Bryson Cale <bryson.cale1@gmail.com>
2022-06-26 09:55:14 -0700

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,16 @@
 pushfirst!(LOAD_PATH,"../src/")
 
 using Documenter
+using EchelleBase
+using EchelleReduce
+using EchelleSpectralModeling
 using Echelle
-#using EchelleBase
-#using EchelleReduce
-#using EchelleSpectralModeling
 
 makedocs(
     sitename = "Echelle.jl",
     format = Documenter.HTML(),
-#    modules = [Echelle, EchelleBase, EchelleReduce, EchelleSpectralModeling],
-    modules = [Echelle],
+    modules = [Echelle, EchelleBase, EchelleReduce, EchelleSpectralModeling],
+#    modules = [Echelle],
     pages = [
         "index.md",
         "spectrographs.md",
