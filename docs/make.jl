1fdfe39edc78e8e55cb633d2255cc0d8f927c894
Bryson Cale <bryson.cale1@gmail.com>
2022-06-26 10:04:05 -0700

    docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,16 @@
 pushfirst!(LOAD_PATH,"../src/")
 
 using Documenter
-println("HERE1")
 using EchelleBase
-println("HERE2")
 using EchelleReduce
-println("HERE3")
 using EchelleSpectralModeling
-println("HERE4")
 using Echelle
-println("HERE5")
 
 makedocs(
     sitename = "Echelle.jl",
     format = Documenter.HTML(),
-    println("HERE6")
-    modules = [Echelle, EchelleBase, EchelleReduce, EchelleSpectralModeling],
-    println("HERE7")
-#    modules = [Echelle],
+#    modules = [Echelle, EchelleBase, EchelleReduce, EchelleSpectralModeling],
+    modules = [],
     pages = [
         "index.md",
         "spectrographs.md",
@@ -33,9 +26,6 @@ makedocs(
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-println("HERE8")
 deploydocs(
     repo = "github.com/astrobc1/Echelle.jl.git"
 )
-
-println("HERE9")
