4f8bee0f6825cf0155905cecfd3a6693cdf7812f
Bryson Cale <bryson.cale1@gmail.com>
2024-04-26 09:29:45 -0700

    nothing changed

--- src/barycenter.jl
+++ src/barycenter.jl
@@ -1,7 +1,7 @@
 export get_barycentric_corrections, get_exposure_midpoint_time
 
 function get_exposure_midpoint_time(data::SpecData, args...; kwargs...)
-    return get_exposure_start_time(data, args...; kwargs...) + (get_itime(data, args...; kwargs...) / (2 * 86400))
+    return get_exposure_start_time(data, args...; kwargs...) + get_itime(data, args...; kwargs...) / (2 * 86400)
 end
 
 function get_barycentric_corrections(jdutc::Real; obs_name::String, star_name::String, zmeas::Real=0)
